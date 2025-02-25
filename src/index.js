31aa4f46a1d30e6b25536ba6c6bc747b12f680ff
Akash Bheke <akashab2362@gmail.com>
2025-02-25 17:03:40 +0530

    Project Setup completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
